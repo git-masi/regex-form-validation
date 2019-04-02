573e83a20884d3a6b39e66d59e7958e91bdd62e3
Eric Masi <eric@gitmasi.com>
2019-04-02 13:59:58 -0400

    add html data- attribute, add event listeners

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const firstName = document.getElementById('first-name'),
-      lastName = document.getElementById('last-name'),
-      zipCode = document.getElementById('zip-code'),
-      phoneNumber = document.getElementById('phone-number'),
-      email = document.getElementById('email'),
-      password = document.getElementById('password'),
-      repeatPassword = document.getElementById('repeat-password'),
-      submitButton = document.querySelector('#validate-form button[type=submit]');
-
+const inputFields = document.querySelectorAll('#validate-form input');
 
 const regexObj = {
-  name: /^[a-z]$/i,
-  zip: /^[0-9]{5}$/,
-  phone: /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
-  email: /^[\w]@[\w].[a-z]$/i,
-  password: /^([0-9]{1,}[A-Z]{1,}[!@#$%^&*-]{1,}[^\s]){8,16}$/
+  'name': /^[a-z]{2,40}$/i,
+  'zip': /^[0-9]{5}$/,
+  'phone': /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
+  'email': /^[\w]@[\w].[a-z]$/i,
+  'password': /^([0-9]{1,}[A-Z]{1,}[!@#$%^&*-]{1,}[^\s]){8,16}$/
 }
+
+function validate(el, regex) {
+  regex = regexObj[regex];
+  if (regex.test(el.value)) {
+    el.classList.remove('invalid');
+    el.classList.add('valid');
+  } else {
+    el.classList.remove('valid');
+    el.classList.add('invalid');
+  }
+}
+
+window.addEventListener('load', () => {
+  inputFields.forEach(el => {
+    let elRegex = el.getAttribute('data-regex');
+    el.addEventListener('blur', () => {
+      validate(el, elRegex);
+    });
+  });
+});
